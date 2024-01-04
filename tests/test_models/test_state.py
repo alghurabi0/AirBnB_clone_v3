680ac74f7c5f7a0b8e7993f0a7c5638c0fafed28
Ubuntu <ubuntu@ip-172-31-84-171.ec2.internal>
2024-01-04 22:27:47 +0000

    pep8

--- tests/test_models/test_state.py
+++ tests/test_models/test_state.py
@@ -8,7 +8,7 @@ import inspect
 import models
 from models import state
 from models.base_model import BaseModel
-import pep8
+from pycodestyle import Checker
 import unittest
 State = state.State
 
@@ -22,16 +22,16 @@ class TestStateDocs(unittest.TestCase):
 
     def test_pep8_conformance_state(self):
         """Test that models/state.py conforms to PEP8."""
-        pep8s = pep8.StyleGuide(quiet=True)
-        result = pep8s.check_files(['models/state.py'])
-        self.assertEqual(result.total_errors, 0,
+        checker = Checker('models/state.py')
+        result = list(checker.check_all())
+        self.assertEqual(len(file_errors), 0,
                          "Found code style errors (and warnings).")
 
     def test_pep8_conformance_test_state(self):
         """Test that tests/test_models/test_state.py conforms to PEP8."""
-        pep8s = pep8.StyleGuide(quiet=True)
-        result = pep8s.check_files(['tests/test_models/test_state.py'])
-        self.assertEqual(result.total_errors, 0,
+        checker = Checker('tests/test_models/test_state.py')
+        result = list(checker.check_all())
+        self.assertEqual(len(file_errors), 0,
                          "Found code style errors (and warnings).")
 
     def test_state_module_docstring(self):
