d75ca3411b45ccdc45467bed807555545b62ea97
Ubuntu <ubuntu@ip-172-31-84-171.ec2.internal>
2024-01-04 22:36:31 +0000

    pep8 fixed

--- tests/test_models/test_state.py
+++ tests/test_models/test_state.py
@@ -8,8 +8,8 @@ import inspect
 import models
 from models import state
 from models.base_model import BaseModel
-from pycodestyle import Checker
 import unittest
+import pep8
 State = state.State
 
 
@@ -22,16 +22,16 @@ class TestStateDocs(unittest.TestCase):
 
     def test_pep8_conformance_state(self):
         """Test that models/state.py conforms to PEP8."""
-        checker = Checker('models/state.py')
-        result = list(checker.check_all())
-        self.assertEqual(len(file_errors), 0,
+        pep8s = pep8.StyleGuide(quiet=True)
+        result = pep8s.check_files(['models/state.py'])
+        self.assertEqual(result.total_errors, 0,
                          "Found code style errors (and warnings).")
 
     def test_pep8_conformance_test_state(self):
         """Test that tests/test_models/test_state.py conforms to PEP8."""
-        checker = Checker('tests/test_models/test_state.py')
-        result = list(checker.check_all())
-        self.assertEqual(len(file_errors), 0,
+        pep8s = pep8.StyleGuide(quiet=True)
+        result = pep8s.check_files(['tests/test_models/test_state.py'])
+        self.assertEqual(result.total_errors, 0,
                          "Found code style errors (and warnings).")
 
     def test_state_module_docstring(self):
@@ -83,7 +83,7 @@ class TestState(unittest.TestCase):
         self.assertEqual(type(new_d), dict)
         self.assertFalse("_sa_instance_state" in new_d)
         for attr in s.__dict__:
-            if attr is not "_sa_instance_state":
+            if attr != "_sa_instance_state":
                 self.assertTrue(attr in new_d)
         self.assertTrue("__class__" in new_d)
 
