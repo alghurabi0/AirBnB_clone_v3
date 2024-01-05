2d261b40db3adfac00e66408a9958a6515d989f0
alghurabi0 <126355218+alghurabi0@users.noreply.github.com>
2024-01-05 03:08:24 +0200

    state view

---
+++ api/v1/views/states.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python3
+""" State restful API """
+from flask import jsonify, abort, request
+from api.v1.views import app_views
+from models import storage
+from models.state import State
+
+
+@app_views.route('/states', methods=['GET'], strict_slashes=False)
+def get_states():
+    """ Get all state objects """
+    states = storage.all(State).values()
+    return jsonify([state.to_dict() for state in states])
+
+@app_views.route('/states/<state_id>', methods=['GET'], strict_slashes=False)
+def get_state(state_id):
+    """ Get state object """
+    state = storage.get(State, state_id)
+    if state:
+        return jsonify(state.to_dict())
+    else:
+        abort(404)
+
+@app_views.route('/states/<state_id>', methods=['DELETE'], strict_slashes=False)
+def delete_state(state_id):
+    """ Delete state object """
+    state = storage.get(State, state_id)
+    if state:
+        storage.delete(state)
+        storage.save()
+        return jsonify({}), 200
+    else:
+        abort(404)
+
+@app_views.route('/states', methods=['POST'], strict_slashes=False)
+def post_state():
+    """ Create state object """
+    if not request.get_json():
+        abort(400, 'Not a JSON')
+    if 'name' not in request.get_json():
+        abort(400, 'Missing name')
+    state = State(**request.get_json())
+    storage.new(state)
+    storage.save()
+    return jsonify(state.to_dict()), 201
+
+@app_views.route('/states/<state_id>', methods=['PUT'], strict_slashes=False)
+def put_state(state_id):
+    """ Update state object """
+    if not request.get_json():
+        abort(400, 'Not a JSON')
+    state = storage.get(State, state_id)
+    if state:
+        for key, value in request.get_json().items():
+            if key not in ['id', 'created_at', 'updated_at']:
+                setattr(state, key, value)
+        storage.save()
+        return jsonify(state.to_dict()), 200
+    else:
+        abort(404)
