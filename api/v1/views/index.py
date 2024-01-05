84888c1e35431d7289b869f66601669ecbe8be2f
Ubuntu <ubuntu@ip-172-31-84-171.ec2.internal>
2024-01-05 18:12:24 +0000

    hello world

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,6 +3,12 @@
 from api.v1.views import app_views
 from flask import jsonify
 from models import storage
+from models.amenity import Amenity
+from models.place import Place
+from models.user import User
+from models.state import State
+from models.city import City
+from models.review import Review
 
 
 @app_views.route('/status', strict_slashes=False)
@@ -14,9 +20,9 @@ def status():
 @app_views.route('/stats', strict_slashes=False)
 def stats():
     """Returns stats"""
-    return jsonify({"amenities": storage.count("Amenity"),
-                    "cities": storage.count("City"),
-                    "places": storage.count("Place"),
-                    "reviews": storage.count("Review"),
-                    "states": storage.count("State"),
-                    "users": storage.count("User")})
+    return jsonify({"amenities": storage.count(Amenity),
+                    "cities": storage.count(City),
+                    "places": storage.count(Place),
+                    "reviews": storage.count(Review),
+                    "states": storage.count(State),
+                    "users": storage.count(User)})
