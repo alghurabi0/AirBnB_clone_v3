f861ae1bdd82730f88f8563b4bdec4adc2188f83
alghurabi0 <126355218+alghurabi0@users.noreply.github.com>
2024-01-07 00:00:54 +0200

    priv pwd

--- models/base_model.py
+++ models/base_model.py
@@ -71,7 +71,7 @@ class BaseModel:
             del new_dict["_sa_instance_state"]
         if getenv("HBNB_TYPE_STORAGE") == "db":
             if "password" in new_dict:
-                del new_dict["password"]
+                del new_dict["_password"]
         return new_dict
 
     def delete(self):
